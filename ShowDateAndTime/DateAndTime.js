709bc46a84828fd0a81d4ebaa55724839e227291
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-10-29 13:44:18 +0530

    display date and time

---
+++ ShowDateAndTime/DateAndTime.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body bgcolor="black">
+<font color="white">
+<h1>Display Date and Time</h1>
+<button type="button"
+id="this"
+onclick="document.getElementById('this').innerHTML = Date()">
+Button</button>
+</font>
+</body>
+</html> 
